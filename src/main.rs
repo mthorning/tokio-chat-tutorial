e014de8344de99e462be0380e607e06a6aed1665
Matthew Thorning <matthew@ometria.com>
2022-11-03 11:05:10 +0000

    multiple client echo server

--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,27 @@ use tokio::{
 async fn main() {
     let listener = TcpListener::bind("localhost:9002").await.unwrap();
 
-    let (mut socket, _addr) = listener.accept().await.unwrap();
-
-    // can split read part from write part so that BufReader can take ownership of read part
-    // and write part can be used within the loop:
-    let (reader, mut writer) = socket.split();
-
-
-    let mut reader = BufReader::new(reader);
-    let mut line = String::new();
-
     loop {
-        let bytes_read = reader.read_line(&mut line).await.unwrap();
-        if bytes_read == 0 {
-            // empty line should disconnect the session
-            break;
-        }
+        let (mut socket, _addr) = listener.accept().await.unwrap();
 
-        writer.write_all(line.as_bytes()).await.unwrap();
-        line.clear();
+        tokio::spawn(async move {
+            // can split read part from write part so that BufReader can take ownership of read part
+            // and write part can be used within the loop:
+            let (reader, mut writer) = socket.split();
+
+            let mut reader = BufReader::new(reader);
+            let mut line = String::new();
+
+            loop {
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+                if bytes_read == 0 {
+                    // empty line should disconnect the session
+                    break;
+                }
+
+                writer.write_all(line.as_bytes()).await.unwrap();
+                line.clear();
+            }
+        });
     }
 }
