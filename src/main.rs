759ae7d61a15468a6d0ff8f26c00f2ccbbbbd440
Matthew Thorning <matthew@ometria.com>
2022-11-03 11:35:26 +0000

    chat server 1

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
+    sync::broadcast,
 };
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:9002").await.unwrap();
 
+    let (tx, mut _rx) = broadcast::channel::<String>(10);
+
     loop {
         let (mut socket, _addr) = listener.accept().await.unwrap();
+
+        // need to clone because tx cannot be moved into the loop
+        let tx = tx.clone();
+
+        // this is strange, can't move rx into the loop below but we don't clone the rx from the
+        // channel, instead we can get an rx from the cloned tx by using subscribe:
+        let mut rx = tx.subscribe();
 
         tokio::spawn(async move {
             // can split read part from write part so that BufReader can take ownership of read part
@@ -19,14 +29,21 @@ async fn main() {
             let mut line = String::new();
 
             loop {
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
-                if bytes_read == 0 {
-                    // empty line should disconnect the session
-                    break;
-                }
+                tokio::select! {
+                    result =  reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            // empty line should disconnect the session
+                            break;
+                        }
 
-                writer.write_all(line.as_bytes()).await.unwrap();
-                line.clear();
+                        tx.send(line.clone()).unwrap();
+                        line.clear();
+                    }
+                    result = rx.recv() => {
+                        let msg = result.unwrap();
+                        writer.write_all(msg.as_bytes()).await.unwrap();
+                    }
+                }
             }
         });
     }
